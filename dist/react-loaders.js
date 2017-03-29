352326995bb1f864fffa957e98087d19126f4bf8
Jon Jaques <jon@3five.com>
2017-03-29 12:25:27 -0500

    Add in demo files

---
+++ dist/react-loaders.js
@@ -0,0 +1,219 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = require("classnames");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Types = exports.Loader = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(1);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _classnames2 = __webpack_require__(0);
+
+var _classnames3 = _interopRequireDefault(_classnames2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Loader = exports.Loader = function (_Component) {
+  _inherits(Loader, _Component);
+
+  function Loader() {
+    _classCallCheck(this, Loader);
+
+    return _possibleConstructorReturn(this, (Loader.__proto__ || Object.getPrototypeOf(Loader)).apply(this, arguments));
+  }
+
+  _createClass(Loader, [{
+    key: 'renderDiv',
+    value: function renderDiv(n) {
+      var styles = this.props.styles || {};
+      if (this.props.color) {
+        styles.backgroundColor = this.props.color;
+      }
+      return _react2.default.createElement('div', { key: n, style: styles });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _classnames;
+
+      var nDivs = range(Types[this.props.type]);
+      var classes = (0, _classnames3.default)((_classnames = {
+        loader: true
+      }, _defineProperty(_classnames, 'loader-' + this.props.size, this.props.size !== 'md'), _defineProperty(_classnames, 'loader-active', this.props.active), _defineProperty(_classnames, 'loader-hidden', !this.props.active), _classnames), this.props.className);
+
+      return _react2.default.createElement(
+        'div',
+        { className: classes },
+        _react2.default.createElement(
+          'div',
+          { className: 'loader-inner ' + this.props.type },
+          nDivs.map(this.renderDiv.bind(this))
+        )
+      );
+    }
+  }], [{
+    key: 'removeType',
+    value: function removeType(type) {
+      delete Types[key];
+    }
+  }, {
+    key: 'addType',
+    value: function addType(key, nDivs) {
+      return Types[key] = nDivs;
+    }
+  }]);
+
+  return Loader;
+}(_react.Component);
+
+Loader.propTypes = {
+  type: _react2.default.PropTypes.string,
+  size: _react2.default.PropTypes.string,
+  active: _react2.default.PropTypes.bool,
+  color: _react2.default.PropTypes.string
+};
+Loader.defaultProps = {
+  type: 'ball-pulse',
+  size: 'md',
+  active: true
+};
+exports.default = Loader;
+var Types = exports.Types = {
+  "ball-pulse": 3,
+  "ball-grid-pulse": 9,
+  "ball-clip-rotate": 1,
+  "ball-clip-rotate-pulse": 2,
+  "square-spin": 1,
+  "ball-clip-rotate-multiple": 2,
+  "ball-pulse-rise": 5,
+  "ball-rotate": 1,
+  "cube-transition": 2,
+  "ball-zig-zag": 2,
+  "ball-zig-zag-deflect": 2,
+  "ball-triangle-path": 3,
+  "ball-scale": 1,
+  "line-scale": 5,
+  "line-scale-party": 4,
+  "ball-scale-multiple": 3,
+  "ball-pulse-sync": 3,
+  "ball-beat": 3,
+  "line-scale-pulse-out": 5,
+  "line-scale-pulse-out-rapid": 5,
+  "ball-scale-ripple": 1,
+  "ball-scale-ripple-multiple": 3,
+  "ball-spin-fade-loader": 8,
+  "line-spin-fade-loader": 8,
+  "triangle-skew-spin": 1,
+  "pacman": 5,
+  "ball-grid-beat": 9,
+  "semi-circle-spin": 1
+};
+
+function range(x) {
+  var i = -1,
+      arr = [];
+  while (++i < x) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+/***/ })
+/******/ ])));
